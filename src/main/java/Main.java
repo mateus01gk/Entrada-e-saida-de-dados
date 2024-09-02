434bff1957fc0d653369a037ae45ae8d7f869459
mateus01gk <169069087+mateus01gk@users.noreply.github.com>
2024-09-02 12:28:31 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+
+//System.out.println("Entre com um número inteiro");
+ //int voltaVida = sc.nextInt();
+ //System.out.println(" o número digitado foi");
+//System.out.println(voltaVida);
+
+ //sc.close();
+
+//System.out.println("Entre com um número racional");
+ //double  voltaVida = sc.nextDouble();
+ //System.out.println(" o número digitado foi");
+//System.out.println(voltaVida);
+ //sc.close();
+
+    System.out.println("Entre com o nome de seu futro filho(a)");
+ String voltaVida = sc.nextString();
+ System.out.println(" o nome digitado foi");
+System.out.println(voltaVida);a
+  
+
+ sc.close();
+
+
+    
+  
+  }
+
+
+}
